dbcc7359ee1c38dc3ffc7efc6e7223a97396291b
kasilianaoliveira <kasilianaoliveira@gmail.com>
2026-03-01 18:22:15 -0300

    feat: add auth_provider column and allow nullable password in users table, update docker-compose for health checks and volume management, and add authlib dependency

--- src/app.py
+++ src/app.py
@@ -1,19 +1,24 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
+from starlette.middleware.sessions import SessionMiddleware
 
 from core.exceptions.error_handlers import register_error_handlers
 from core.logging_config import setup_logging
+from core.settings import get_settings
+from routers.admin_daily_limit_router import protected_admin_daily_router
 from routers.appointments_router import (
     protected_admin_router as appointments_admin_router,
+)
+from routers.appointments_router import (
     protected_user_router as appointments_user_router,
 )
-from routers.services_router import public_services_router, services_router
-from routers.storage_router import router as storage_router
-from routers.user_router import protected_user_router, user_public_router
-from routers.admin_daily_limit_router import protected_admin_daily_router
 from routers.auth_router import auth_public_router
+from routers.services_router import public_services_router, services_router
+from routers.user_router import protected_user_router, user_public_router
 
 setup_logging()
+
+config = get_settings()
 
 
 def create_app() -> FastAPI:
@@ -21,11 +26,16 @@ def create_app() -> FastAPI:
         title="Appointment",
         description="API for appointment services",
     )
+    app.add_middleware(
+        SessionMiddleware,  # ty:ignore[invalid-argument-type]
+        secret_key=config.SECRET_KEY.get_secret_value()
+    )
+
     app.include_router(user_public_router)
     app.include_router(protected_user_router)
-    
+
     app.include_router(auth_public_router)
-    
+
     app.include_router(public_services_router)
     app.include_router(services_router)
 
@@ -33,7 +43,7 @@ def create_app() -> FastAPI:
     app.include_router(appointments_admin_router)
 
     app.include_router(protected_admin_daily_router)
-    #app.include_router(storage_router)
+    # app.include_router(storage_router)
 
     register_error_handlers(app)
 
